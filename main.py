3a8dc0fd4dff708ee5af95905b78f3f5d1a1987c
RubenChavez04 <rubenchavez2022@gmail.com>
2025-02-01 13:22:30 -0600

    Setup

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import sys
+import json
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,
+    QPushButton, QFrame, QMenu, QInputDialog, QColorDialog, QFileDialog, QMessageBox
+)
+from PyQt6.QtCore import Qt, QPoint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
